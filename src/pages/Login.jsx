2df7e4d1368008e56a44b6f57414bdb0ddb80e14
Oghenetega Etareh <tega.etare@gmail.com>
2023-08-18 23:43:02 -0400

    feature/login and register page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,72 @@
-
+import { useState } from "react"
 export default function Login() {
- 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        // if((username.length<4) ||(password.length<5)){
+        //     setUsername('')
+        //     setPassword('')
+        //     return(setError("Username and Password must be eight characters or more"))
+        // }
+        try {
+            const response = await fetch('https://strangers-things.herokuapp.com/api/2302-acc-et-web-pt-a/users/login', 
+              { 
+                method: "POST", 
+                headers: { 
+                  "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify({ 
+                  user: {
+                    username: username, 
+                    password: password
+                  }
+                }) 
+              })
+            const result = await response.json();
+            setUsername('')
+            setPassword('')
+            
+            console.log(result);
+            if(!result.success){
+                setError(result.error.message)
+                console.log(result.error.message)
+            }
+
+            setSuccessMessage(result.data.message)
+            // setToken(result.data.token)
+            //console.log("Token: ", result.data.token) 
+            console.log(result.data.message)
+            //console.log("Error is ", error)
+            
+        } catch (error) {
+          //setError(error.message);
+        }
         
-      
+      }
 
 
     return (
 
         <>
-            <h1>Login Page</h1>
-          
+            <h1>Login</h1>
+            <div>
+                {successMessage && <p className="success"> {successMessage}</p>}     
+                {error && <p className='error'>{error}</p>} 
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </label>
+                    <label>
+                        Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                    <button>Submit</button>
+                </form>
+
+            </div>
         </>
 
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,62 @@
+import { useState } from "react"
 export default function Register() {
- 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        // if((username.length<8) ||(password.length<8)){
+        //     setUsername('')
+        //     setPassword('')
+        //     return(setError("Username and Password must be eight characters or more"))
+        // }
+        try {
+            const response = await fetch('https://strangers-things.herokuapp.com/api/2302-acc-et-web-pt-a/users/register', 
+              { 
+                method: "POST", 
+                headers: { 
+                  "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify({ 
+                  user: {
+                    username: username, 
+                    password: password
+                  }
+                }) 
+              })
+            const result = await response.json();
+            // setToken(result.data.token)
+            console.log(result);
+            console.log("Token: ", result.data.token)
+            setUsername('')
+            setPassword('')
+        } catch (error) {
+            setError(error.message);
+            console.log(error)
+        }
         
-      
+      }
 
 
     return (
 
         <>
-            <h1>Register Page</h1>
-          
+            <h1>Register</h1>
+            <div>
+                
+                {error && <p className='error'>{error}</p>} 
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </label>
+                    <label>
+                        Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                    <button>Submit</button>
+                </form>
+
+            </div>
         </>
 
 
