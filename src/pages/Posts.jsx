7dc67e1465f3e9bd89c4ecd779553aa0581c6511
Oghenetega Etareh <tega.etare@gmail.com>
2023-08-20 01:25:24 -0400

    Feature/Search Bar added to posts page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react"
 import { fetchPosts } from "../API/APIFunctions";
 import PostCard from "../components/PostCard";
 import PostsForm from "../components/PostsForm";
-export default function Posts({token}) {
+export default function Posts({ token }) {
     const [posts, setPosts] = useState([])
+    const [filteredPosts, setFilteredPosts] = useState([]);
 
     async function fetchData() {
         const data = await fetchPosts()
         setPosts(data.posts)
+        setFilteredPosts(data.posts)
         console.log("Data: ", data.posts);
 
 
@@ -15,24 +17,37 @@ export default function Posts({token}) {
     useEffect(() => {
         fetchData()
     }, [])
+    function handleSearch(e){
+        //code goes here
+        e.preventDefault()
+        const search = e.target.value
+        const filteredPosts = posts.filter((post) => {
+            return post.title.toLowerCase().includes(search.toLowerCase())
+        })
+        setFilteredPosts(filteredPosts)
+    }
 
 
     return (
 
         <>
-             {token && <PostsForm token={token} fetchData={fetchData} />}
+            {token && <PostsForm token={token} fetchData={fetchData} />}
+            <form onSubmit={handleSearch}>
+                <label htmlFor="search">Search Posts</label>
+                <input onChange={handleSearch} type="text" id="search" />
+            </form>
             <h1>Posts</h1>
             <main>
-            {
-                posts.map((post) => (
-                    <PostCard 
-                        key={post._id}
-                        post={post}
-                        fetchPosts={fetchData}
-                        token = {token}
+                {
+                    filteredPosts.map((post) => (
+                        <PostCard
+                            key={post._id}
+                            post={post}
+                            fetchPosts={fetchData}
+                            token={token}
                         />
-                ))
-            }
+                    ))
+                }
             </main>
         </>
 
